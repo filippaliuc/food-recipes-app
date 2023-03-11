b61b36cef7eb8a0f7c99bf5841cff54f54b9fd1c
Filip <paliucfilip@yahoo.com>
2023-03-11 12:37:15 +0200

    Added firebase configuration to the project, to connect it with firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        projectId: 'food-recipes-app-5d498',
+        appId: '1:242622028615:web:f1ce2fafefe565ae7e4a62',
+        storageBucket: 'food-recipes-app-5d498.appspot.com',
+        apiKey: 'AIzaSyAF08rDv0ycFGKiLMFS-hK0KKcqUDa9OH0',
+        authDomain: 'food-recipes-app-5d498.firebaseapp.com',
+        messagingSenderId: '242622028615',
+        measurementId: 'G-KRB30694HS',
+    },
+}
